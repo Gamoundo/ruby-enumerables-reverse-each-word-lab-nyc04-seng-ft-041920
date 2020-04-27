6aa16b4e2acb83352e818966f76f5af46bb5188b
Akil Griffiths <akilgg@gmail.com>
2020-04-27 15:01:50 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,8 @@
- def reverse_each_word(str)
-words = []
-words.push(str)
+def reverse_each_word(str)
+array= []
+words = str.split
+words.each do |word| 
+array.push(word.reverse)
+end
+array.join(" ")
+end
